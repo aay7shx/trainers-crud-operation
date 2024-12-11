5eec5995ae792649e6c96284b0c1ff9e772f2f7d
Aayush <aayushborkar.3114@gmail.com>
2024-12-11 14:02:39 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -65,23 +65,23 @@
 //     console.log(`Server is running on http://localhost:${port}`)
 // })
 
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
+// import express from 'express'
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+// import dotenv from 'dotenv'
 
-dotenv.config();
+// dotenv.config();
 
-const app= express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const port = process.env.PORT
+// const app= express();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const port = process.env.PORT
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','index.html'));
-})
-app.use(express.static(path.join(__dirname,'views')))
+// app.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname,'views','index.html'));
+// })
+// app.use(express.static(path.join(__dirname,'views')))
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`Server is running on http://localhost:${port}`)
+// })
