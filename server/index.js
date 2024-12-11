d43394c1dee63709547037d5a165090875fa2e07
Aayush <aayushborkar.3114@gmail.com>
2024-12-11 13:58:44 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -64,3 +64,24 @@
 // app.listen(port,()=>{
 //     console.log(`Server is running on http://localhost:${port}`)
 // })
+
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app= express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = process.env.PORT
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','index.html'));
+})
+app.use(express.static(path.join(__dirname,'views')))
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
