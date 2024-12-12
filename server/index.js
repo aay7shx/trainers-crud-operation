b2f1cc0f34fb7a2b094f83f296845db8ae50d48b
Aayush <aayushborkar.3114@gmail.com>
2024-12-12 13:04:18 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -77,11 +77,77 @@
 // const __dirname = path.dirname(__filename);
 // const port = process.env.PORT
 
-// app.get("/",(req,res)=>{
-//     res.sendFile(path.join(__dirname,'views','index.html'));
+// // app.get("/",(req,res)=>{
+// //     // res.sendFile(path.join(__dirname,'views','index.html'));
+// // })
+
+// app.use((req,res,next)=>{
+//     const secretCode = req.query.secret;
+//     if(secretCode='1234'){
+//         req.isAuthorized=true;
+//     }
+//     else{
+//         req.isAuthorized=false;
+//     }
+//   next(); 
 // })
+
+// app.get("/",()=>{
+//     if(req.isAuthorized){
+//         res.sendFile((path.join(__dirname,'views','index.html')))
+//     }
+//     else{
+//         res.send("Unauthorized")
+//     }})
+
 // app.use(express.static(path.join(__dirname,'views')))
 
 // app.listen(port,()=>{
 //     console.log(`Server is running on http://localhost:${port}`)
 // })
+
+
+
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
+const app =express();
+app.use(express.json());
+
+dotenv.config();
+const port=process.env.Port;
+
+
+app.use(function num(req,res,next){
+const secretcode=req.query.secret;
+
+if(secretcode==='1234')
+{
+    req.isAuthorized=true;
+}
+else
+{
+    req.isAuthorized=false;
+}
+next();
+});
+
+app.get("/",(req,res)=>{
+   if (req.isAuthorized)
+   {
+    res.sendFile(path.join(__dirname,"views","index.html"));
+   }
+   else
+   {
+    res.send("Unauthorized");
+   }
+})
+
+//****************************************************************** 
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
